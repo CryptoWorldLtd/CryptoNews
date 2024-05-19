1d247dadbe915b9660f0522ba6e519f977ddfe56
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2024-05-19 15:33:49 +0300

    Added project structure
    dsasdjasjdla

--- CryptoWorld.News/CryptoWorld.News.Server/WeatherForecast.cs
+++ CryptoWorld.News/CryptoWorld.News.Server/WeatherForecast.cs
@@ -4,7 +4,7 @@ namespace CryptoWorld.News.Server
     {
         public DateOnly Date { get; set; }
 
-        public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; } 
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
