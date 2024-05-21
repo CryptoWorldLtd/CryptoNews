77a5dde5f7cc6f8c43f5dab289eacbd0ffff6dd3
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2024-05-21 19:39:15 +0300

    test

--- CryptoWorld.News/CryptoWorld.News.Server/WeatherForecast.cs
+++ CryptoWorld.News/CryptoWorld.News.Server/WeatherForecast.cs
@@ -6,6 +6,9 @@ namespace CryptoWorld.News.Server
 
         public int TemperatureC { get; set; }
             
+
+
+
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
