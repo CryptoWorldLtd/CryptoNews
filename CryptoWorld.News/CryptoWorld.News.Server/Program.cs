b09301fc543318fb6da2339cc3c0aa5725aeb99f
vmmateev <v.m.mateev@gmail.com>
2024-05-29 00:22:52 +0300

    InitialCreate Migration
    There is few warnings with foreign keys on the initial migration, maybe because e.g. Category model has no CategoryID and in Article model we use CategoryID and Category<Category>

--- CryptoWorld.News/CryptoWorld.News.Server/Program.cs
+++ CryptoWorld.News/CryptoWorld.News.Server/Program.cs
@@ -1,4 +1,8 @@
+using CryptoWorld.News.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
