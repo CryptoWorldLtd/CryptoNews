fb4dede06d8395bdb0d2fb29cdcd39799d3a76f8
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-06-04 09:48:12 +0300

    Create Login and Register Models

---
+++ CryptoWorld.News/CryptoWorld.News.Server/Models/LoginRequestModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CryptoWorld.Application.Server.Models
+{
+    public class LoginRequestModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
