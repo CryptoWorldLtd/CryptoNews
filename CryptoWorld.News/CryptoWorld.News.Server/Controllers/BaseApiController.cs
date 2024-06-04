ccc9f0af802ec383520b0a1495cd3e08769630df
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-06-04 09:47:25 +0300

    Create BaseApiController

---
+++ CryptoWorld.News/CryptoWorld.News.Server/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CryptoWorld.Application.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
