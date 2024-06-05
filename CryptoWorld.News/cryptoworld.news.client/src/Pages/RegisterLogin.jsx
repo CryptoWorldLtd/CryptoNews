3e42a9f3bae5e1f0c80fdd169eaeebe2c3a43f5f
Rosen Roydev <rosen.roydev@gmail.com>
2024-06-05 16:52:52 +0300

    value props for name and email are added to be sure that program reacts when user type something

--- CryptoWorld.News/cryptoworld.news.client/src/Pages/RegisterLogin.jsx
+++ CryptoWorld.News/cryptoworld.news.client/src/Pages/RegisterLogin.jsx
@@ -1,12 +1,22 @@
-import React, {useState} from 'react'
-import './RegisterLogin.css'
+import React, { useState } from 'react';
+import './RegisterLogin.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
 
- const RegisterLogin = () => {
+const RegisterLogin = () => {
     
-  const[action,setAction] = useState("Register");
+  const [action, setAction] = useState("Register");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
   return (
     <div className='container'>
@@ -15,18 +25,33 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
         <div className='underline'></div>
       </div>
       <div className='inputs'>
-        {action==="Login"? <div></div> : <div className='input'>
-          <i>
-            <FontAwesomeIcon icon={faUser} />
-          </i>
-          <input type='text' placeholder='Name' />
-        </div>}
-     
-        <div className='input'>
-          <i>
-            <FontAwesomeIcon icon={faEnvelope} />
-          </i>
-          <input type='email' placeholder='Email' />
+        {action==="Login"? <div></div> : 
+          <div className='input-group'>
+            <div className='input'>
+              <i>
+                <FontAwesomeIcon icon={faUser} />
+              </i>
+              <input 
+                type='text' 
+                placeholder='Name' 
+                value={name} 
+                onChange={handleNameChange} 
+              />
+            </div>
+          </div>
+        }
+        <div className='input-group'>
+          <div className='input'>
+            <i>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </i>
+            <input 
+              type='email' 
+              placeholder='Email' 
+              value={email} 
+              onChange={handleEmailChange} 
+            />
+          </div>
         </div>
         <div className='input'>
           <i>
@@ -36,11 +61,11 @@ import { faLock } from '@fortawesome/free-solid-svg-icons';
         </div>
         {action==="Register"?<div></div> : <div className='forgot-password'>Forgot password? <span>Click here</span></div>}
         <div className='submit-container'>
-            <div className={action==="Login"?"submit gray" : "submit"} onClick={()=>{setAction("Register")}}>Register</div>
-            <div className={action==="Register"?"submit gray" : "submit"} onClick={()=>{setAction("Login")}} >Login</div>
+          <div className={action==="Login"?"submit gray" : "submit"} onClick={()=>{setAction("Register")}}>Register</div>
+          <div className={action==="Register"?"submit gray" : "submit"} onClick={()=>{setAction("Login")}} >Login</div>
         </div>
       </div>
     </div>
   );
 }
-export default RegisterLogin
+export default RegisterLogin;
