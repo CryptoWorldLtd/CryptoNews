87286cfc8bfa7a6e6328f0eb505cd6c7e3739f40
Rosen Roydev <rosen.roydev@gmail.com>
2024-06-10 22:19:43 +0300

    db.json created to save the dates from console, Register.jsx modified to handle dates from console

--- CryptoWorld.News/cryptoworld.news.client/src/Pages/Register.jsx
+++ CryptoWorld.News/cryptoworld.news.client/src/Pages/Register.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import './Register.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
 
 const Register = () => {
-    
   const [action, setAction] = useState("Register");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
   const handleNameChange = (event) => {
     setName(event.target.value);
@@ -19,6 +20,26 @@ const Register = () => {
     setEmail(event.target.value);
   };
 
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:3001/register', {
+        name,
+        email,
+        password
+      });
+      console.log(response.data);
+      // Handle success (e.g., redirect or show success message)
+    } catch (error) {
+      console.error(error);
+      setError("Registration failed");
+      // Handle error (e.g., show error message)
+    }
+  };
+
   return (
     <div className='container'>
       <div className="header">
@@ -26,19 +47,19 @@ const Register = () => {
         <div className='underline'></div>
       </div>
       <div className='inputs'>
-          <div className='input-group'>
-            <div className='input'>
-              <i>
-                <FontAwesomeIcon icon={faUser} />
-              </i>
-              <input 
-                type='text' 
-                placeholder='Name' 
-                value={name} 
-                onChange={handleNameChange} 
-              />
-            </div>
+        <div className='input-group'>
+          <div className='input'>
+            <i>
+              <FontAwesomeIcon icon={faUser} />
+            </i>
+            <input 
+              type='text' 
+              placeholder='Name' 
+              value={name} 
+              onChange={handleNameChange} 
+            />
           </div>
+        </div>
         
         <div className='input-group'>
           <div className='input'>
@@ -57,15 +78,26 @@ const Register = () => {
           <i>
             <FontAwesomeIcon icon={faLock} />
           </i>
-          <input type='password' placeholder='Password' />
+          <input 
+            type='password' 
+            placeholder='Password' 
+            value={password}
+            onChange={handlePasswordChange}
+          />
         </div>
       
         <div className='submit-container'>
-          <div className={action==="Login"?"submit gray" : "submit"} onClick={()=>{setAction("Register")}}>Register</div>
-    
+          <div 
+            className={action === "Login" ? "submit gray" : "submit"} 
+            onClick={handleSubmit}
+          >
+            Register
+          </div>
         </div>
+        {error && <div className="error">{error}</div>}
       </div>
     </div>
   );
 }
+
 export default Register;
