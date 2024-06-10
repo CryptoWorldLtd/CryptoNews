7325cd569a0b33fb5bde8d05483443937eb81962
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-06-10 16:56:41 +0300

    Add validation attributes in RegisterRequestModel

--- CryptoWorld.News/CryptoWorld.News.Core/ViewModels/RegisterRequestModel.cs
+++ CryptoWorld.News/CryptoWorld.News.Core/ViewModels/RegisterRequestModel.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CryptoWorld.News.Core.ViewModels
 {
     public class RegisterRequestModel
     {
         [Required]
+        [MinLength(4, ErrorMessage ="The username should be at least 4 characters long.")]
         public string Username { get; set; }
         [Required]
         [EmailAddress]
@@ -12,6 +14,7 @@ namespace CryptoWorld.News.Core.ViewModels
         [Required]
         public string Password { get; set; }
         [Required]
+        [Compare(nameof(Password), ErrorMessage = "The password and confirmed password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
