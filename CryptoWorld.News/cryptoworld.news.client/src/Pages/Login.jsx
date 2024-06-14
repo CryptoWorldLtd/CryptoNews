f14a4d8046b0b223be44ee4ad8a65bc97d832698
Rosen Roydev <rosen.roydev@gmail.com>
2024-06-14 13:52:02 +0300

    Login send post request to back-end by axios

--- CryptoWorld.News/cryptoworld.news.client/src/Pages/Login.jsx
+++ CryptoWorld.News/cryptoworld.news.client/src/Pages/Login.jsx
@@ -1,60 +1,102 @@
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom"
-import './Login.css'
+import axios from 'axios';
+import {  faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope } from '@fortawesome/free-regular-svg-icons';
-import { faLock } from '@fortawesome/free-solid-svg-icons';
-import axios from "axios";
+import { faUser, faLock , faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import './Login.css';
 
 const Login = () => {
-    const[values,setValues] = useState({
-        username: "",
-        password: "",
-        showPassword: false
-    });
-    const handlePasswordVisibility = () =>{
-        setValues({
-            ...values,
-            showPassword: !values.showPassword
-        });
+  const [email, setEmail] = useState(() => (localStorage.checkbox ? localStorage.email : ""));
+  const [password, setPassword] = useState(() => (localStorage.checkbox ? localStorage.password : ""));
+  const [isChecked, setIsChecked] = useState(() => !!localStorage.checkbox);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handlePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (isChecked) {
+      localStorage.email = email;
+      localStorage.password = password;
+      localStorage.checkbox = isChecked ? "1" : "";
+    } else {
+      localStorage.removeItem('email');
+      localStorage.removeItem('password');
+      localStorage.removeItem('checkbox');
     }
-   
-    return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Welcome Back</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                <i>
-                <FontAwesomeIcon icon={faUser} />
-              </i>
-              <input 
-                type='text' 
-                placeholder='Name' 
-                onChange={(e)=>setValues({...values,username:e.target.value})} 
-              />
-                </div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                <i>
-            <FontAwesomeIcon icon={faLock} />
-          </i>
-          <input 
-                type={values.showPassword ? "text" : "password"} 
-                placeholder='Password' 
-                onChange={(e)=>setValues({...values,password:e.target.value})} 
-              />
-                </div>
-            </div>
-            <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
-            <div className="submit-container">
-                <div className="submit">Sign Up</div>
-                <div className="submit">Login</div>
-            </div>
+
+    try {
+      console.log('Submitting:', { email, password });
+      const response = await axios.post('https://localhost:7249/account/login', {
+        email,
+        password
+      }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log('Response:', response.data);
+      alert('Login successful!');
+    } catch (error) {
+      console.error(error);
+      alert('Login failed. Please try again.');
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Welcome Back</div>
+        <div className="underline"></div>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="inputs">
+          <div className="input">
+            <i>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </i>
+            <input 
+              type="email" 
+              placeholder="Email" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} 
+            />
+          </div>
         </div>
-    )
+        <div className="inputs">
+          <div className="input">
+            <i>
+              <FontAwesomeIcon icon={faLock} />
+            </i>
+            <input 
+              type={showPassword ? "text" : "password"} 
+              placeholder="Password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} 
+            />
+            <i onClick={handlePasswordVisibility} className='toggle-password'>
+              <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye}/>
+            </i>
+          </div>
+        </div>
+        <div className="remember-me">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            name="lsRememberMe"
+            onChange={(e) => setIsChecked(e.target.checked)}
+          />
+          <label htmlFor="lsRememberMe"> Remember me</label>
+        </div>
+        <div className="forgot-password">Forgot Password? <span>Click Here!</span></div>
+        <div className="submit-container">
+          <button type="submit" className="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
 };
-export default Login
+
+export default Login;
