38c94637c0b90f2c370e0a19d2862b39659fb518
Rosen Roydev <rosen.roydev@gmail.com>
2024-06-24 15:25:04 +0300

    Added service, view Model and interface for HomePage

---
+++ CryptoWorld.News/CryptoWorld.News.Core/Interfaces/IHomeNewsService.cs
@@ -0,0 +1,14 @@
+﻿using CryptоWorld.News.Core.ViewModels.Home_Page;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptоWorld.News.Core.Interfaces
+{
+    public interface IHomeNewsService
+    {
+        Task<HomePageNewsModel> HomePageNewsAsync(HomePageNewsModel model);
+    }
+}
---
+++ CryptoWorld.News/CryptoWorld.News.Core/ViewModels/Home Page/HomePageNewsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptоWorld.News.Core.ViewModels.Home_Page
+{
+    public class HomePageNewsModel
+    {
+        [Required]
+        string Title {  get; set; } 
+
+        [Required]
+        string Content { get; set; }
+
+        public string ImageUrl {  get; set; }
+
+        public string DateTime { get; set; }
+
+    }
+}
