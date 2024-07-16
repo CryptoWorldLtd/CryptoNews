6a46eae413d2fee9d83b1b67b2f2241e65f70240
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-07-16 11:59:30 +0300

    Add property Region to Article entity

--- CryptoWorld.News/CryptoWorld.News.Core/ViewModels/HomePage/FilteredNewsModel.cs
+++ CryptoWorld.News/CryptoWorld.News.Core/ViewModels/HomePage/FilteredNewsModel.cs
@@ -5,8 +5,9 @@ namespace CryptoWorld.News.Core.ViewModels.HomePage
     public class FilteredNewsModel
     {
         public const int NewsPerPage = 5;
-        public string Category { get; init; } = null!;
-        public string SearchTerm { get; init; } = null!;
+        public string Category { get; init; }
+        public string Region { get; init; }
+        public string SearchTerm { get; init; }
         public NewsSorting Sorting { get; init; }
         public int CurrentPage { get; init; } = 1;
     }
--- CryptoWorld.News/CryptoWorld.News.Core/ViewModels/HomePage/PageNewsModel.cs
+++ CryptoWorld.News/CryptoWorld.News.Core/ViewModels/HomePage/PageNewsModel.cs
@@ -2,19 +2,22 @@
 {
     public class PageNewsModel
     {
-        public PageNewsModel(string title, string content, string imageUrl, string datePublished, int rating)
+        public PageNewsModel(string title, string content, string imageUrl, string datePublished, int rating, string region)
         {
             this.Title= title; 
             this.Content= content;
             this.ImageUrl= imageUrl;
             this.DatePublished= datePublished;
             this.Rating = rating;
+            this.Region = region;
         }
       
         public string Title {  get; set; } 
         public string Content { get; set; }
         public string ImageUrl {  get; set; }
         public string DatePublished { get; set; }
+        public string Category {  get; set; }
         public int Rating { get; set; }
+        public string Region { get; set; }
     }
 }
