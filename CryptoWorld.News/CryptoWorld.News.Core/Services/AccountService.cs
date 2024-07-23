0e8105a708af0002218af25bd90f4f1fcf5421ae
Rosen Roydev <rosen.roydev@gmail.com>
2024-07-23 15:29:27 +0300

    Account service is updated with try catch blocks for all methods

--- CryptoWorld.News/CryptoWorld.News.Core/Services/AccountService.cs
+++ CryptoWorld.News/CryptoWorld.News.Core/Services/AccountService.cs
@@ -10,6 +10,8 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Text.RegularExpressions;
 using System.Security.Claims;
 using System.Text;
+using Serilog;
+using System.ComponentModel;
 
 namespace CryptoWorld.News.Core.Services
 {
@@ -28,154 +30,232 @@ namespace CryptoWorld.News.Core.Services
 
         public async Task<IdentityResult> RegisterAsync(RegisterRequestModel model)
         {
-            var userExists = await userManager.FindByEmailAsync(model.Email);
-
-            if (userExists != null)
-                throw new ArgumentException("User with such email already exists.");
-
-            if (!IsValidEmail(model.Email))
-                throw new ArgumentException("Invalid email address format.");
-
-            var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-            var result = await userManager.CreateAsync(user, model.Password);
-
-            var confirmationToken = await userManager.GenerateEmailConfirmationTokenAsync(user);
-            confirmationToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(confirmationToken));
-            string action = "confirmemail";
-            var emailBody = GenerateConfirmationLink(action, confirmationToken, model.Email);
-
-            if (result.Succeeded)
+            try
             {
-                await this.signInManager.SignInAsync(user, false);
-                await emailSenderService.SendEmailAsync(model.Email, model.Username, emailBody);
+                var userExists = await userManager.FindByEmailAsync(model.Email);
+
+                if (userExists != null)
+                    throw new ArgumentException("User with such email already exists.");
+
+                if (!IsValidEmail(model.Email))
+                    throw new ArgumentException("Invalid email address format.");
+
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                var confirmationToken = await userManager.GenerateEmailConfirmationTokenAsync(user);
+                confirmationToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(confirmationToken));
+                string action = "confirmemail";
+                var emailBody = GenerateConfirmationLink(action, confirmationToken, model.Email);
+
+                if (result.Succeeded)
+                {
+                    await this.signInManager.SignInAsync(user, false);
+                    await emailSenderService.SendEmailAsync(model.Email, model.Username, emailBody);
+                }
+                return result;
             }
-            return result;
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred during user registration.");
+                return IdentityResult.Failed(new IdentityError
+                {
+                    Description =
+                    "An error occurred during registration. Please try again later."
+                });
+            }
+
         }
 
         public async Task<LoginResponseModel> LoginAsync(LoginRequestModel model)
         {
-            var user = await userManager.FindByEmailAsync(model.Email);
-
-            if (user == null)
-                throw new ArgumentException("There is no such user.");
-
-            var result = await this.signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
-
-            if (!result.Succeeded)
-                throw new ArgumentException("There was a error while loggin you in! Please try again later or contact an administrator.");
-
-            if (!user.EmailConfirmed)
-                throw new ArgumentException("Email address not confirmed!");
-
-            var token = GenerateJwtToken(user);
-            return new LoginResponseModel()
+            try
             {
-                Token = token,
-                Email = model.Email,
-                Id = user.Id.ToString()
-            };
+                var user = await userManager.FindByEmailAsync(model.Email);
+
+                if (user == null)
+                    throw new ArgumentException("There is no such user.");
+
+                var result = await this.signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (!result.Succeeded)
+                    throw new ArgumentException("There was a error while loggin you in! Please try again later or contact an administrator.");
+
+                if (!user.EmailConfirmed)
+                    throw new ArgumentException("Email address not confirmed!");
+
+                var token = GenerateJwtToken(user);
+                return new LoginResponseModel()
+                {
+                    Token = token,
+                    Email = model.Email,
+                    Id = user.Id.ToString()
+                };
+            }
+            catch (ArgumentException ex)
+            {
+                Log.Warning(ex, ex.Message);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Problem occurred during user login", ex);
+                throw new InvalidOperationException("An error occurred during login. Please try again later.");
+            }
         }
 
         public async Task<IdentityResult> VerifyEmailAsync(string token, string email)
         {
-            if (!IsValidEmail(email))
-                throw new ArgumentException("Invalid email address format.");
+            try
+            {
+                if (!IsValidEmail(email))
+                    throw new ArgumentException("Invalid email address format.");
 
-            var user = await userManager.FindByEmailAsync(email);
+                var user = await userManager.FindByEmailAsync(email);
 
-            if (user == null)
-                throw new ArgumentException("There is no such user.");
+                if (user == null)
+                    throw new ArgumentException("There is no such user.");
 
-            var decodedTokenBytes = WebEncoders.Base64UrlDecode(token);
-            var decodedToken = Encoding.UTF8.GetString(decodedTokenBytes);
-            var result = await userManager.ConfirmEmailAsync(user, decodedToken);
+                var decodedTokenBytes = WebEncoders.Base64UrlDecode(token);
+                var decodedToken = Encoding.UTF8.GetString(decodedTokenBytes);
+                var result = await userManager.ConfirmEmailAsync(user, decodedToken);
 
-            if (!result.Succeeded)
-                throw new ArgumentException("Incorrect email.");
+                if (!result.Succeeded)
+                    throw new ArgumentException("Incorrect email.");
 
-            return (result);
+                return (result);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred during verification of mail!");
+                throw;
+            }
         }
 
         public async Task<IdentityResult> PasswordResetAsync(string token, string email, string newPassword)
         {
-            if (!IsValidEmail(email))
-                throw new ArgumentException("Invalid email address format.");
-
-            var user = await userManager.FindByEmailAsync(email);
-            if (user == null)
-                throw new ArgumentException("There is no such user.");
-
-            var result = await userManager.ResetPasswordAsync(user, token, newPassword);
-
-            if (!result.Succeeded)
+            try
             {
-                throw new ArgumentException("Password reset failed.");
-            }
+                if (!IsValidEmail(email))
+                    throw new ArgumentException("Invalid email address format.");
 
-            return (result);
+                var user = await userManager.FindByEmailAsync(email);
+                if (user == null)
+                    throw new ArgumentException("There is no such user.");
+
+                var result = await userManager.ResetPasswordAsync(user, token, newPassword);
+
+                if (!result.Succeeded)
+                {
+                    throw new ArgumentException("Password reset failed.");
+                }
+
+                return (result);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred during reset password");
+                throw;
+            }
         }
 
         public async Task<IdentityResult> GeneratePasswordResetToken(string email)
         {
-            if (!IsValidEmail(email))
-                throw new ArgumentException("Invalid email address format.");
+            try
+            {
+                if (!IsValidEmail(email))
+                    throw new ArgumentException("Invalid email address format.");
 
-            var user = await userManager.FindByEmailAsync(email);
+                var user = await userManager.FindByEmailAsync(email);
 
-            if (user == null)
-                throw new ArgumentException("There is no such user.");
+                if (user == null)
+                    throw new ArgumentException("There is no such user.");
 
-            var resetPassToken = await userManager.GeneratePasswordResetTokenAsync(user);
-            var encodedToken = Uri.EscapeDataString(resetPassToken);
-            string action = "resetpassword";
-            var passwordResetLink = GenerateConfirmationLink(action, encodedToken, email);
-            await emailSenderService.SendEmailAsync(user.Email, user.UserName, passwordResetLink);
+                var resetPassToken = await userManager.GeneratePasswordResetTokenAsync(user);
+                var encodedToken = Uri.EscapeDataString(resetPassToken);
+                string action = "resetpassword";
+                var passwordResetLink = GenerateConfirmationLink(action, encodedToken, email);
+                await emailSenderService.SendEmailAsync(user.Email, user.UserName, passwordResetLink);
 
-            return IdentityResult.Success;
+                return IdentityResult.Success;
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred during generating of password reset token", ex);
+                return IdentityResult.Failed(new IdentityError
+                {
+                    Description =
+                    "An error occurred during generating of password reset token"
+                });
+            }
         }
 
         private string GenerateJwtToken(ApplicationUser user)
         {
-            var claims = new[]
+            try
             {
+                var claims = new[]
+              {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(ClaimTypes.Email, user.Email)
-            };
-            var secretKey = "abcdefghijklmnopqrstuvwxyzabcdef";
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+              };
+                var secretKey = "abcdefghijklmnopqrstuvwxyzabcdef";
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
 
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(30),
-                signingCredentials: creds);
+                var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occured during generating Jwt token");
+                throw;
+            }
         }
 
         private bool IsValidEmail(string email)
         {
-            if (string.IsNullOrWhiteSpace(email))
-                return false;
-            var emailPattern = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
+            try
+            {
+                if (string.IsNullOrWhiteSpace(email))
+                    return false;
+                var emailPattern = @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$";
 
-            return Regex.IsMatch(email, emailPattern);
+                return Regex.IsMatch(email, emailPattern);
+            }
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred during during validation of mail!");
+                throw;
+            }
         }
 
         private string GenerateConfirmationLink(string action, string token, string email)
         {
-            var confirmationLink = string.Empty;
-
-            if (action == "resetpassword")
+            try
             {
-                confirmationLink = $"https://localhost:5173/{action}/{token}/{email}";
+                var confirmationLink = string.Empty;
+
+                if (action == "resetpassword")
+                {
+                    confirmationLink = $"https://localhost:5173/{action}/{token}/{email}";
+                    return confirmationLink;
+                }
+
+                confirmationLink = $"https://localhost:7249/Account/{action}?token={token}&email={email}";
                 return confirmationLink;
             }
-
-            confirmationLink = $"https://localhost:7249/Account/{action}?token={token}&email={email}";
-            return confirmationLink;
+            catch (Exception ex)
+            {
+                Log.Error("An error occurred during generating confirmation link");
+                throw;
+            }
         }
     }
 }
