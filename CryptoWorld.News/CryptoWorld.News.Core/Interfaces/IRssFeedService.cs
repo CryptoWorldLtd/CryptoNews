6c99eecfbf8f7dbd5fa2d773a3fb77bf2a1d9750
Hristina Glavcheva <glavchevahristina@gmail.com>
2024-08-13 12:19:03 +0300

    Small fix of IRssFeedService

--- CryptoWorld.News/CryptoWorld.News.Core/Interfaces/IRssFeedService.cs
+++ CryptoWorld.News/CryptoWorld.News.Core/Interfaces/IRssFeedService.cs
@@ -5,5 +5,7 @@ namespace CryptoWorld.News.Core.Interfaces
     public interface IRssFeedService
     {
         public Task<List<RssResponseModel>> GetFeedItemsAsync();
+
+        public Task CreateNewsFromRssFeedAsync(List<RssResponseModel> models);
     }
 }
