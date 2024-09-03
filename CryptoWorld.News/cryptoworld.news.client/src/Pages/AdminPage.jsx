1e7bc5d292aff8a97d268318bd28c4a138d8d87a
Rosen Roydev <rosen.roydev@gmail.com>
2024-09-03 13:12:53 +0300

    Changed color text of the table

--- CryptoWorld.News/cryptoworld.news.client/src/Pages/AdminPage.jsx
+++ CryptoWorld.News/cryptoworld.news.client/src/Pages/AdminPage.jsx
@@ -1,5 +1,6 @@
 import  React from 'react';
 import { DataGrid } from '@mui/x-data-grid'; 
+import './AdminPage.css';
 
 const columns = [
   { field: 'userId', headerName: 'UserID', width: 70 },
@@ -27,7 +28,7 @@ const rows = [
   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 28 },
   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
